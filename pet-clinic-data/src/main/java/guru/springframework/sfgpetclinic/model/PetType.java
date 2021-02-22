1a9b0854d3064cb80a9c91ef989b82302b0ab4ff
mankokolya <mankokolya@gmail.com>
2021-02-22 12:25:56 +0200

    convert owner to jpa entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -18,18 +18,6 @@ import javax.persistence.Table;
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
-    @Builder
-    public PetType(Long id, String name) {
-        super(id);
-        this.name = name;
-    }
-
     @Column(name = "name")
     private String name;
-
-
-    @Override
-    public String toString() {
-        return name;
-    }
 }
